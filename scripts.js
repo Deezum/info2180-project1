ef06fe88edd5de533fe3a7480483b21902a5ce2b
Deezum <beadleselena@gmail.com>
2021-10-21 01:34:20 -0500

    artificial.png

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function() {
+
+    var display = document.getElementsByClassName("message");
+    var button = document.querySelector("form > button");
+   
+   
+
+    button.onclick = function(de) {
+        email = document.querySelector("input").value;
+        if(email.includes("@") && email.includes(".")) {
+            display[0].textContent = "Thank you! Your email address " + email + " has been added to our mailing list!";
+            event.preventDefault();
+        } else {
+            display[0].textContent = "Please enter a valid email address.";
+            de.preventDefault();
+        }
+    };
+})/* Add your JavaScript to this file */
